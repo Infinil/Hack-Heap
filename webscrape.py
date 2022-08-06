e488e7ceb1c596be64e87063da0b0f74aff92d47
Infinil <main8user8@gmail.com>
2022-08-07 01:13:19 +0530

    Changing to selenium

--- webscrape.py
+++ webscrape.py
@@ -1,8 +1,11 @@
+from concurrent.futures import process
 from appwrite.client import Client
 from appwrite.services.databases import Databases
 from appwrite.query import Query
 import dateparser
-import scrapy
+import selenium
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 def main(request, response):
   client : Client = Client()
@@ -12,13 +15,13 @@ def main(request, response):
   .set_key(request.env['SECRET_KEY']) 
   )
   database : Databases = Databases(client, database_id='default')
-  web_scrape_obj = WebScrape(database, request)
+  web_scrape_obj = WebScrapeFunctions(database, request)
   web_scrape_obj.devfolio()
   web_scrape_obj.hackerearth()
   web_scrape_obj.mlh()
   return response.send('Function Execution Completed.')
 
-class WebScrape:
+class WebScrapeFunctions:
   def __init__(self, database: Databases, request):
     self.database = database
     self.request = request
@@ -38,10 +41,23 @@ class WebScrape:
       )
 
   def devfolio(self):
-    source = 'devfolio'
+    source = 'Devfolio'
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://devfolio.co/hackathons")
   
   def hackerearth(self):
-    source = 'hackerearth'
+    source = 'Hackerearth'
   
   def mlh(self):
-    source = 'mlh'
+    source = 'MLH'
+
+class Request:
+  env = {
+    'API_ENDPOINT' : 'http://backend.limitnil.com/v1',
+    'DATABASE_ID' : 'default',
+    'PROJECT_ID' : 'hackheap',
+    'SECRET_KEY' : '8dd766283f12dc44b71e5b152e5e2cd373a964f20dd4d4086fdd48c5c41b4f5dc8a351fb28db6762fb8beefac063d302fbef1492081ae5fdb587444739fa8cfaf4e247b9ed17618c4b88e2271b3b7a0af8e14f21eb5e0b1d79458047750d6cba5d8b5988715df099511b3cd7f3af32ca35e1a8ef08ded1d049b4dde31bc38cf3',
+    'COLLECTION_ID' : 'hackathons',
+  }
+
+main(Request(),2)
