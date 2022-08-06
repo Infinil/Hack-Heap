b25950312d71b98c82386768379ea5222b3eba42
Infinil <main8user8@gmail.com>
2022-08-06 23:44:09 +0530

    Skeleton code for scrapy

---
+++ scrapy.py
@@ -0,0 +1,36 @@
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.query import Query
+import dateparser
+import scrapy
+
+def main(request, response):
+  client : Client = Client()
+  (client
+  .set_endpoint(request.env['API_ENDPOINT'])
+  .set_project(request.env['PROJECT_ID'])
+  .set_key(request.env['SECRET_KEY']) 
+  )
+  database : Databases = Databases(client, database_id='default')
+  web_scrape_obj = WebScrape(database, request)
+  web_scrape_obj.devfolio()
+  web_scrape_obj.hackerearth()
+  web_scrape_obj.mlh()
+  return response.send('Function Execution Completed.')
+
+class WebScrape:
+  def __init__(self, database: Databases, request):
+    self.database = database
+    self.request = request
+  
+  def common_operations(self, dict_data: dict):
+    pass
+
+  def devfolio(self):
+    pass
+  
+  def hackerearth(self):
+    pass
+  
+  def mlh(self):
+    pass
